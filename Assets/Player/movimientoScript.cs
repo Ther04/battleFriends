387f0b461a84d6eaad1791207b9792af2f61900c
Ther04 <tacs590@gmail.com>
2024-08-26 14:09:17 -0400

    Initial commit

---
+++ Assets/Player/movimientoScript.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movimientoScript : MonoBehaviour
+{
+    [Header("movimiento")]
+    public float speed = 5f;
+    public float jumpForce = 2f;
+    public float moverHorizontal; 
+    private Rigidbody2D rb;
+    private Vector3 velocidad = Vector3.zero;
+    [Range(0, 0.3f)] public float suavizado;
+    private bool isGrounded = true;
+    private bool derecha = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moverHorizontal = Input.GetAxis("Horizontal");
+        
+        saltar();
+    }
+
+    private void FixedUpdate()
+    {
+        mover();
+    }
+
+    void mover()
+    {
+        
+
+        if(moverHorizontal < 0 && derecha)
+        {
+            Vector3 escala = transform.localScale;
+            escala.x *= -1;
+            transform.localScale = escala;
+            derecha = false;
+        }else if(moverHorizontal > 0 && !derecha)
+        {
+            Vector3 escala = transform.localScale;
+            escala.x *= -1;
+            transform.localScale = escala;
+            derecha = true;
+        }
+        Vector2 movimiento = new Vector2(moverHorizontal * speed, rb.velocity.y);
+        rb.velocity = movimiento;
+        //rb.velocity = Vector3.SmoothDamp(rb.velocity, new Vector2(moverHorizontal * speed, rb.velocity.y), ref velocidad, suavizado);
+    }
+
+    void saltar()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+        }
+    }
+
+    void girar()
+    {
+
+    }
+}

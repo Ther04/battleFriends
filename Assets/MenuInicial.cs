0d8ce4c57e9acd875a2c926b0f3d523f7c36e0ab
MDome07 <155572614+MDome07@users.noreply.github.com>
2024-09-10 09:52:57 -0400

    creación menú inicial

---
+++ Assets/MenuInicial.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuInicial : MonoBehaviour
+{
+    public void Jugar()
+    {
+
+    }
+
+    public void Instrucciones()
+    {
+
+    }
+    public void Salir()
+    {
+        Application.Quit();
+        Debug.Log("Saliendo");
+    }
+}

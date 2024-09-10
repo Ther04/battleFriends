88e26ecf6e6ebcf337ce6e6560d114576232fe04
MDome07 <155572614+MDome07@users.noreply.github.com>
2024-09-10 18:19:52 -0400

    Botones Instrucciones

--- Assets/MenuInicial.cs
+++ Assets/MenuInicial.cs
@@ -1,9 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class MenuInicial : MonoBehaviour
 {
+    public GameObject firstButton;
+    public GameObject secondButton;
+    private bool buttonSelected = false;
+    private bool pressed = true;
+    void Update()
+    {
+        if (Input.anyKeyDown&&buttonSelected==false)
+        {
+            EventSystem.current.SetSelectedGameObject(firstButton);
+            buttonSelected = true;
+        }else if(Input.anyKeyDown && pressed==false)
+        {
+            EventSystem.current.SetSelectedGameObject(secondButton);
+            pressed=true;
+        }
+    }
     public void Jugar()
     {
         Debug.Log("Jugar");
@@ -12,6 +29,13 @@ public class MenuInicial : MonoBehaviour
     public void Instrucciones()
     {
         Debug.Log("Instrucciones");
+        pressed = false;
+        buttonSelected = true;
+    }
+    public void Volver() {
+        Debug.Log("Menú");
+        pressed = true;
+        buttonSelected = false;
     }
     public void Salir()
     {

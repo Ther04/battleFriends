871f333e5901692f4c174e62af31ffe605331021
MDome07 <155572614+MDome07@users.noreply.github.com>
2024-09-11 11:16:30 -0400

    Botones de selección de pjs

---
+++ Assets/PjInfo.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PjInfo : MonoBehaviour
+{
+    public int posX;
+    public int posY;
+    public string charId;
+}
---
+++ Assets/ScreenManagerSelect.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenManagerSelect : MonoBehaviour
+{
+    public int numPlayers = 2;
+    //public List<PlayerInterfaces>
+    public int maxX;
+    public int maxY;
+}
